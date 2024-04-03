99e807af2dd9a447f3e2833c32912cb6b12f7f1f
Edgar <edgarasige@gmail.com>
2024-04-03 23:19:52 +0300

    Completed assingments

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+    let userData = {
+        name: name,
+        email: email
+    };
+
+    let config = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    };
+
+    return fetch('http://localhost:3000/users', config)
+        .then(response => response.json())
+        .then(data => {
+            const newId = data.id;
+            document.body.innerHTML += `<p>New ID: ${newId}</p>`;
+        })
+        .catch(error => {
+            document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+        });
+}
+
+
+
